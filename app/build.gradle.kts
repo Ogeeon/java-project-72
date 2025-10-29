64a53ad9c31361c73b3639893e1cec8b49a6c272
Alexey Bogatyrev <avbogatyrev@gmail.com>
2025-10-29 08:17:57 +0000

    Add initial App.java and Dockerfile

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,20 +2,32 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
+    application
     java
     jacoco
     id("com.github.ben-manes.versions") version "0.50.0"
     id("org.sonarqube") version "7.0.1.6134"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
+
+application {
+    mainClass.set("hexlet.code.App")
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation("org.slf4j:slf4j-simple:2.0.17")
+    implementation("gg.jte:jte:3.2.0")
+    implementation("io.javalin:javalin:6.6.0")
+    implementation("io.javalin:javalin-bundle:6.6.0")
+    implementation("io.javalin:javalin-rendering:6.6.0")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,34 @@
+package hexlet.code;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinJte;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class App {
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "7070");
+        log.info("App started, listening on port " + port);
+        return Integer.parseInt(port);
+    }
+
+    public static void main(String[] args) throws IOException, SQLException {
+        var app = getApp();
+
+        app.start(getPort());
+    }
+
+    public static Javalin getApp() throws IOException, SQLException {
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+            config.fileRenderer(new JavalinJte());
+        });
+
+        app.get("/", ctx -> ctx.result("Hello, world!"));
+
+        return app;
+    }
+}
